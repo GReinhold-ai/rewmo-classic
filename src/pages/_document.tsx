6bfc39e96716fc76a29927ab7cc181066172d26a
Gary Reinhold <gary@example.comGReinhold@rewmo.ai>
2025-06-02 12:50:44 -0700

    Add Impact.com site verification meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#ffffff" />
+        {/* Impact.com verification */}
+        <meta name="impact-site-verification" value="2010818e-5e4c-4322-ab03-007b902645a6" />
       </Head>
       <body>
         <Main />
